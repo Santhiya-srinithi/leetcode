2ab9328b3a927f42a3733fe9ce038a5d1ea132a6
Santhiya-srinithi <santhiyasrinithi1510@gmail.com>
2026-07-17 15:03:37 +0530

    Create 185-DepartmentTopThreeSalaries

---
+++ 185-DepartmentTopThreeSalaries/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,13 @@
+-- Last updated: 7/17/2026, 3:03:35 PM
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId
+      AND e2.salary > e.salary
+) < 3;
